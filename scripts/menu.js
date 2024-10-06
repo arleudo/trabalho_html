dda8bf8c7ac4dfc22e020d5ad00c9cf4922c4f8f
arleudo <antonioarleudo@gmail.com>
2024-10-06 11:34:38 -0300

    ajustes

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,11 @@
 document.querySelector(".menu-toggle").addEventListener("click", function () {
   document.querySelector(".nav-container").classList.toggle("menu-active");
 });
+
+function closeDialog() {
+  document.getElementById('dialogOverlay').style.display = 'none';
+}
+
+function openDialog() {
+  document.getElementById('dialogOverlay').style.display = 'flex';
+}
